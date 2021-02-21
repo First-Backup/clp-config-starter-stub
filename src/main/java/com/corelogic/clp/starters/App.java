537476716bea23cc045158f2ac1dd6836b030f40
DeveloperPrince <prince.maposa@ciibo.com>
2021-02-21 06:09:56 +0200

    changes

---
+++ src/main/java/com/corelogic/clp/starters/App.java
@@ -0,0 +1,13 @@
+package com.corelogic.clp.starters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Yes, I'm Starter!" );
+    }
+}
